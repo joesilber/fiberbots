855e716b2e4dfd37dc9655a2df0051ff6b94ac7b
Joe Silber <josephsilber@gmail.com>
2021-10-12 15:31:13 -0700

    fixed some refs in fvcmeasure.py

--- bin/control/fvcmeasure.py
+++ bin/control/fvcmeasure.py
@@ -12,14 +12,16 @@ import math
 this_file_dir = os.path.realpath(os.path.dirname(__file__))
 os.chdir(this_file_dir)
 sys.path.append('../../modules')
+sys.path.append('../../modules/camera')
 import globals as gl
+import fvchandler
 
 # Measurement default parameters
 defaults = gl.fvc_defaults.copy()
 
 # Command line args for standalone mode, and defaults
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-c', '--camera', type=str, default=defaults['camera'], help=f'fiber view camera to use, valid options are {cameras.keys()}') 
+parser.add_argument('-c', '--camera', type=str, default=defaults['camera'], help=f'fiber view camera to use, valid options are {fvchandler.cameras.keys()}') 
 parser.add_argument('-n', '--num_dots', type=int, default=1, help=f'number of dots to centroid')
 parser.add_argument('-r', '--num_repeats', type=int, default=1, help=f'number of times to repeat the measurement')
 parser.add_argument('-p', '--plot', action='store_true', help='plot measured centroids')
@@ -46,12 +48,12 @@ if __name__ == '__main__':
     for key in ['camera', 'exptime', 'fitbox', 'sim_errmax', 'sim_badmatchfreq']:
         params[key] = getattr(inputs, key)
     logger.info(f'Initializing fvchandler with parameters: {params}')
-    f = FVCHandler(params=params,
-                   take_darks=inputs.take_darks,
-                   save_images=inputs.save_images,
-                   save_biases=inputs.save_biases,
-                   printfunc=logger.info,
-                  ) 
+    f = fvchandler.FVCHandler(params=params,
+                              take_darks=inputs.take_darks,
+                              save_images=inputs.save_images,
+                              save_biases=inputs.save_biases,
+                              printfunc=logger.info,
+                             ) 
     f.min_energy = -math.inf  # suppress checks on dot quality here, since standalone mode often for setup
     xy = []
     peaks = []
